4bac35fe20f4712efa0c2e1e6a57840b70bfc4b3
ThePaniniMan <prmaringantiacts@gmail.com>
2024-03-27 21:05:45 -0400

    match and pit screens json

--- app/screens/matchscreen.py
+++ app/screens/matchscreen.py
@@ -1,4 +1,137 @@
 from kivy.uix.screenmanager import Screen
+from kivy.properties import NumericProperty
+import json
+from kivy.clock import Clock
+from kivy.app import App
+from .homescreen import MainScreen
+
+main_screen = MainScreen()
+current_scouter = main_screen.current_scouter
 
 class MatchScreen(Screen):
-    pass
+    auton_speaker = NumericProperty(0)
+    auton_amp = NumericProperty(0)
+    teleop_speaker = NumericProperty(0)
+    teleop_amp = NumericProperty(0)
+
+    def increment1(self):
+        self.auton_speaker += 1
+
+    def decrement1(self):
+        if self.auton_speaker > 0:
+            self.auton_speaker -= 1
+
+    def increment2(self):
+        self.auton_amp += 1
+    
+    def decrement2(self):
+        if self.auton_amp > 0:
+            self.auton_amp -= 1
+
+    def increment3(self):
+        self.teleop_speaker += 1
+    
+    def decrement3(self):
+        if self.teleop_speaker > 0:
+            self.teleop_speaker -= 1
+    
+    def increment4(self):
+        self.teleop_amp += 1
+    
+    def decrement4(self):
+        if self.teleop_amp > 0:
+            self.teleop_amp -= 1
+    
+
+    def __init__(self, **kwargs):
+        super(MatchScreen, self).__init__(**kwargs)
+        self.starting_place = ""
+        self.note_pickup = ""
+        self.left_starting_zone = ""
+        self.trap = ""
+        self.harmonized = ""
+        self.climbed = ""
+        self.robot_driving = ""
+        self.defense_capabilities = ""
+
+    def set_starting_place(self, value):
+        self.starting_place = value
+
+    def set_note_pickup(self, value):
+        self.note_pickup = value
+
+    def set_left_starting_zone(self, value):
+        self.left_starting_zone = value
+
+    def set_trap(self, value):
+        self.trap = value
+
+    def set_harmonized(self, value):
+        self.harmonized = value
+
+    def set_climbed(self, value):
+        self.climbed = value
+
+    def set_robot_driving(self, value):
+        self.robot_driving = value
+
+    def set_defense_capabilities(self, value):
+        self.defense_capabilities = value
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+    def submit_data(self):
+        
+        data = {
+            'name': current_scouter,
+            'starting_place': self.starting_place,
+            'note_pickup': self.note_pickup,
+            'left_starting_zone': self.left_starting_zone,
+            'auton_speaker': self.auton_speaker,
+            'auton_amp': self.auton_amp,
+            'teleop_speaker': self.teleop_speaker,
+            'teleop_amp': self.teleop_amp,
+            'trap': self.trap,
+            'harmonized': self.harmonized,
+            'climbed': self.climbed,
+            'robot_driving': self.robot_driving,
+            'defense_capabilities': self.defense_capabilities,
+            'other_notes': self.ids.other_notes.text,
+        }
+
+        file_path = 'output.json'
+        with open(file_path, 'r') as f:
+            try:
+                existing_data = json.load(f)
+            except json.JSONDecodeError:
+                existing_data = []
+
+        existing_data.append(data)
+
+        with open(file_path, 'w') as f:
+            f.write('[\n')
+            for i, item in enumerate(existing_data):
+                f.write(json.dumps(item) + (',' if i < len(existing_data) - 1 else '') + '\n')
+            f.write(']\n')
+
+        self.reset_values()
+        app = App.get_running_app()
+        old_screen = app.root.get_screen(self.name)
+        app.root.remove_widget(old_screen)
+        new_screen = MatchScreen(name=self.name)
+        app.root.add_widget(new_screen)
+        app.root.current = new_screen.name
+
+    def reset_values(self):
+        self.auton_speaker = 0
+        self.auton_amp = 0
+        self.teleop_speaker = 0
+        self.teleop_amp = 0
+        self.starting_place = ""
+        self.note_pickup = ""
+        self.left_starting_zone = ""
+        self.trap = ""
+        self.harmonized = ""
+        self.climbed = ""
+        self.robot_driving = ""
+        self.defense_capabilities = ""
+        self.ids.other_notes.text = ""
+
--- app/screens/pitscreen.py
+++ app/screens/pitscreen.py
@@ -1,11 +1,102 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.textfield import MDTextField
+from kivy.app import App
+import json
+from .homescreen import MainScreen
 
+main_screen = MainScreen()
+current_scouter = main_screen.current_scouter
+#shooting speed?
 
 class PitScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(PitScreen, self).__init__(**kwargs)
+        self.drive_train = ""
+        self.fit_under_stage = ""
+        self.intake_description = ""
+        self.scoring_position = ""
+        self.hang_or_park = ""
+        self.buddy_climb = ""
+        self.score_via_trap = ""
+        self.under_stage = ""
 
+    def reset_values(self):
+        self.drive_train = ""
+        self.fit_under_stage = ""
+        self.intake_description = ""
+        self.scoring_position = ""
+        self.hang_or_park = ""
+        self.buddy_climb = ""
+        self.score_via_trap = ""
+        self.under_stage = ""
+
+
+    def set_drive_train(self, value):
+        self.drive_train = value
+        print(self.drive_train)
+        print("uashfda")
+
+    def set_fit_under_stage(self, value):
+        self.fit_under_stage = value
+
+    def set_intake_description(self, value):
+        self.intake_description = value
+
+    def set_scoring_position(self, value):
+        self.scoring_position = value
+
+    def set_hang_or_park(self, value):
+        self.hang_or_park = value
+
+    def set_buddy_climb(self, value):
+        self.buddy_climb = value
+
+    def set_score_via_trap(self, value):
+        self.score_via_trap = value
+
+    def set_under_stage(self, value):
+        self.under_stage = value
+
+    def submit_data(self):
+        print(self.drive_train)
+        data = {
+            'name': current_scouter,
+            'team_number': self.ids.team_number_pit.text,
+            'drive_train': self.drive_train,
+            'fit_under_stage': self.fit_under_stage,
+            'intake_description': self.intake_description,
+            'scoring_position': self.scoring_position,
+            'shooting_location': self.ids.shooting_location_pit.text,
+            'hang_or_park': self.hang_or_park,
+            'buddy_climb': self.buddy_climb,
+            'score_via_trap': self.score_via_trap,
+            'under_stage': self.under_stage,
+            'other_notes': self.ids.additional_comments_pit.text,
+        }
+
+        file_path = 'outputpit.json'
+        with open(file_path, 'r') as f:
+            try:
+                existing_data = json.load(f)
+            except json.JSONDecodeError:
+                existing_data = []
+
+        existing_data.append(data)
+
+        with open(file_path, 'w') as f:
+            f.write('[\n')
+            for i, item in enumerate(existing_data):
+                f.write(json.dumps(item) + (',' if i < len(existing_data) - 1 else '') + '\n')
+            f.write(']\n')
+
+        self.reset_values()
+        app = App.get_running_app()
+        old_screen = app.root.get_screen(self.name)
+        app.root.remove_widget(old_screen)
+        new_screen = PitScreen(name=self.name)
+        app.root.add_widget(new_screen)
+        app.root.current = new_screen.name
 
 
 
